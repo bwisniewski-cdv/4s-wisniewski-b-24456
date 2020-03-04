e64c8b958b2f5bb64814fdea064da398e939b54f
Bartłomiej Wiśniewski <bwisniewski@edu.cdv.pl>
2020-03-04 15:27:09 +0100

    04.03

--- Programowanie strukturalne/test.py
+++ Programowanie strukturalne/test.py
@@ -1,1 +1,30 @@
 print('CDV')
+print(10)
+'''
+komentarz
+blokowy
+'''
+#potegowanie
+potega=2 ** 10
+print(potega)
+
+#pobieranie danych z klawiatury
+name=input()
+# konkatencja+
+print("Twoje imie: " +name)
+surname=input()
+print("Twoje imie: " +name+", nazwisko: "+surname)
+
+'''
+Użytkownik podaje dane z klawiautry: swój wiek podajemy w formacie Twój wiek:Data
+'''
+print('Podaj wiek:',end="")
+age=input()
+print(type(age))
+print("Twój wiek ",age," lata")
+
+surname="Kowalski"
+firstLetter = len(surname)
+print(length)
+
+lastLetter=surname[-1]
